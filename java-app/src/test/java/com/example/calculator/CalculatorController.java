19639c7e9c5de55e062780e7c3235d965df8c549
devopszlpus <dev450276@gmail.com>
2024-12-08 19:29:34 +0530

    Update CalculatorController.java

--- java-app/src/test/java/com/example/calculator/CalculatorController.java
+++ java-app/src/test/java/com/example/calculator/CalculatorController.java
@@ -1,4 +1,4 @@
-package com.example.calculatorapp;
+package com.example.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
